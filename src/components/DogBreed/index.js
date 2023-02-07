d8b2318aeb8f47e03ff99f64a6e1fdc6da5ea28a
Wardasharif <Warda_Sharif@hotmail.co.uk>
2023-02-06 20:28:43 -0500

    changes

--- src/components/DogBreed/index.js
+++ src/components/DogBreed/index.js
@@ -1,26 +1,26 @@
-import React from "react";
-import Personality from "../Personality";
+import React from 'react'
+import Personality from '../Personality'
 
 const DogBreed = ({ breed }) => {
-    return (
-        <div className="dog-card">
-            <img alt={breed.breed} src={breed.photo}></img>
-            <h5> {breed.breed} </h5>
-            <h6 className="dog-header"> Origin </h6>
-            <p> {breed.origin} </p>
-            <h6 className="dog-header"> Popularity level </h6>
-            <p> {breed.popularity}% </p>
-            <ul className="list-unstyled">
-                <h6 className="dog-header"> Personalities </h6>
-                    {breed.temperament.map(temp => {
-                        return <Personality personality={temp}></Personality>
-                    })}
-            </ul>
-        </div>
-    )
+  return (
+    <div className="dog-card">
+      <img alt={breed.breed} src={breed.photo}></img>
+      <h5> {breed.breed} </h5>
+      <h6 className="dog-header"> Origin </h6>
+      <p> {breed.origin} </p>
+      <h6 className="dog-header"> Popularity level </h6>
+      <p> {breed.popularity}% </p>
+      <ul className="list-unstyled">
+        <h6 className="dog-header"> Personalities </h6>
+        {breed.temperament.map((temp) => {
+          return <Personality personality={temp}></Personality>
+        })}
+      </ul>
+    </div>
+  )
 }
 
-export default ;
+export default DogBreed
 
 /*
  {
